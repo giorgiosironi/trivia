113f0dd738dca0187f0234705f64883fb43aa213
Giorgio Sironi <info@giorgiosironi.com>
2013-06-29 10:34:06 +0200

    Added GameTest::testASinglePlayerCanRollTheDice

--- php/GameTest.php
+++ php/GameTest.php
@@ -20,6 +20,21 @@ class GameTest extends PHPUnit_Framework_TestCase
         ));
     }
 
+    public function testASinglePlayerCanRollTheDice()
+    {
+        $this->game->add('Giorgio');
+        $this->clearOutput();
+
+        $this->game->roll(1);
+        $this->assertOutputIs(array(
+            'Giorgio is the current player',
+            'They have rolled a 1',
+            'Giorgio\'s new location is 1',
+            'The category is Science',
+            'Science Question 0',
+        ));
+    }
+
     private function assertOutputIs(array $lines)
     {
         $this->assertEquals(
@@ -37,5 +52,10 @@ class GameTest extends PHPUnit_Framework_TestCase
                 }
             }
         };
+    }
+
+    private function clearOutput()
+    {
+        $this->output = array();
     }
 }
